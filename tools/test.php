9897b427185a8f639de834e6689d889bd390db63
YohanK2 <amorsito0614@gmail.com>
2026-07-14 03:41:28 -0500

    definitivo este si

--- tools/test.php
+++ tools/test.php
@@ -1,3 +1,7 @@
 <?php
-echo "PHP FUNCIONA";
-?>
+require_once __DIR__ . '/../backend/core/auth.php';
+
+echo "<pre>";
+echo "SCRIPT_NAME: " . $_SERVER['SCRIPT_NAME'] . PHP_EOL;
+echo "dirname(): " . dirname($_SERVER['SCRIPT_NAME']) . PHP_EOL;
+echo "getBaseUrl(): " . getBaseUrl() . PHP_EOL;
